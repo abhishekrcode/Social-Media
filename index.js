7c3427477328f781581b11a9e12009d031a8a1e5
Abhishek Raj <abhishek.mintu@gmail.com>
2022-09-17 19:41:46 +0530

    intalling ejs and setup view engine and then create home for view

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require ('express');
 const port = 8000;
 const app = express();
 
+
+
 app.use('/',require('./routes'));
+
+//setup view engine always after midware
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 app.listen(port,function(err){
